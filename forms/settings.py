f72c7ccda884ae62fba1874931e5f212818c4e47
Jashandeep Singh <jashandeep1659@gmail.com>
2023-03-16 20:57:08 +0530

    static'

--- forms/settings.py
+++ forms/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 import os
+import sys
 
 from pathlib import Path
 
@@ -128,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 # Default primary key field type
